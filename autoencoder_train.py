451f52fa018efa9635889313151a08b80d3fb629
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-02-23 01:26:39 +0900

    copying_from_stable_diffusion_github

---
+++ autoencoder_train.py
@@ -0,0 +1,32 @@
+import wandb
+
+import os
+from torch import optim, nn, utils, Tensor
+from data.base import FaceDatasetTrain
+from models.autoencoder import VQModelInterface, AutoencoderKL
+
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+
+# init the autoencoder
+autoencoder = AutoencoderKL(embed_dim=3, monitor="val/rec_loss")
+
+# setup data
+batch_size = 32
+dataset = FaceDatasetTrain()
+train_loader = utils.data.DataLoader(dataset, shuffle=True)
+
+# initialise the wandb logger and name your wandb project
+wandb_logger = WandbLogger(project='fmface_generator')
+
+# add your batch size to the wandb config
+wandb_logger.experiment.config["batch_size"] = batch_size
+
+# pass wandb_logger to the Trainer 
+trainer = pl.Trainer(logger=wandb_logger, benchmark= True, accumulate_grad_batches=2)
+
+# train the model
+trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+
+# [optional] finish the wandb run, necessary in notebooks
+wandb.finish()
---
+++ data/base.py
@@ -0,0 +1,70 @@
+import os
+from abc import abstractmethod
+from PIL import Image
+from torch.utils.data import Dataset, IterableDataset
+from torchvision import transforms
+
+# fix torch random seed
+#torch.manual_seed(0)
+
+class ImgIterableBaseDataset(IterableDataset):
+    '''
+    Define an interface to make the IterableDatasets for text2img data chainable
+    '''
+    def __init__(self, num_records=0, valid_ids=None, size=256):
+        super().__init__()
+        self.num_records = num_records
+        self.valid_ids = valid_ids
+        self.sample_ids = valid_ids
+        self.size = size
+
+        print(f'{self.__class__.__name__} dataset contains {self.__len__()} examples.')
+
+    def __len__(self):
+        return self.num_records
+
+    @abstractmethod
+    def __iter__(self):
+        pass
+
+class FaceDataset(Dataset):
+    def __init__(self, data_dir="/content/DF11", train=True):
+        # get a list of images
+        filenames = os.listdir(data_dir)
+
+        # get the full path to images
+        self.full_filenames = [os.path.join(data_dir, f) for f in filenames]
+
+        if train:
+            self.transform = transforms.Compose([
+                transforms.Resize((256,256)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(), # Scales data into [0,1] 
+                transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1] 
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.Resize((256,256)),
+                transforms.ToTensor(), # Scales data into [0,1] 
+                transforms.Lambda(lambda t: (t * 2) - 1) # Scale between [-1, 1] 
+            ])
+
+    def __len__(self):
+        # return size of dataset
+        return len(self.full_filenames)
+
+    def __getitem__(self, idx):
+        # open image, apply transforms and return with label
+        image = Image.open(self.full_filenames[idx]).convert("RGB")
+        new_img = Image.new(image.mode, (310, 310), (255,255,255))
+        new_img.paste(image, (25, 0))
+        new_img = self.transform(new_img)
+        return new_img
+
+class FaceDatasetTrain(FaceDataset):
+    def __init__(self, **kwargs):
+        super().__init__(data_dir="/content/DF11", train=True)
+
+class FaceDatasetTest(FaceDataset):
+    def __init__(self, **kwargs):
+        super().__init__(data_dir="", train=False)
