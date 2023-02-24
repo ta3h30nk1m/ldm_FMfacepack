c4f765b53a9f1a85ec571681c15d8f9f2527998a
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-02-24 19:47:56 +0900

    add default configuration

--- autoencoder_train.py
+++ autoencoder_train.py
@@ -3,34 +3,84 @@ import wandb
 import os
 import torch
 from torch import optim, nn, utils, Tensor
-from data.base import FaceDatasetTrain
+from data.base import FaceDataset
 from models.autoencoder import AutoencoderKL
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 
-# setup data
-batch_size = 32
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-dataset = FaceDatasetTrain()
-train_loader = utils.data.DataLoader(dataset, shuffle=True)
+def parse_args(argv=None):
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default='/content/DF11',
+                        help='dataset path, default set for colab env')
+    parser.add_argument('--checkpoint_path', type=str, default='/content/drive/MyDrive/ldm_fmface/checkpoint',
+                        help='checkpoint saving path, default set for colab env')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='batch size')
 
-# init the autoencoder
-autoencoder = AutoencoderKL(embed_dim=3, monitor="val/rec_loss").to(device)
 
-# initialise the wandb logger and name your wandb project
-wandb_logger = WandbLogger(project='fmface_generator')
+    parser.add_argument('--embed_dim', type=int, default=3,
+                        help='intermediate dimension (input dim of latent diffusion)')
+    parser.add_argument('--z_channel', type=int, default=3,
+                        help='output&input dim for encoder&decoder, usually same as embed_dim')
+    parser.add_argument('--resolution', type=int, default=256,
+                        help='input image size')
+    parser.add_argument('--in_channels', type=int, default=3,
+                        help='input dim for encoder')
+    parser.add_argument('--out_ch', type=int, default=3,
+                        help='output dim for decoder')
+    parser.add_argument('--ch', type=int, default=128,
+                        help='intermediate channel num')
+    parser.add_argument('--ch_mult', type=str, default='1,2,4',
+                        help='number of channels of each layer in encoder/decoder')
+    parser.add_argument('--num_res_blocks', type=int, default=2,
+                        help='resnet block number')
+    parser.add_argument('--dropout', type=float, default=0.0,
+                        help='percentage of dropout')
+    args = parser.parse_args(argv)
+    return args
 
-# add your batch size to the wandb config
-wandb_logger.experiment.config["batch_size"] = batch_size
 
-# pass wandb_logger to the Trainer 
-trainer = pl.Trainer(logger=wandb_logger, benchmark= True, accumulate_grad_batches=2, default_root_dir="/content/drive/MyDrive/ldm_fmface", accelerator="gpu" if device=='cuda' else 'cpu', devices=1)
+def main(args):
+    # setup data
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    dataset = FaceDataset(data_dir=args.data_path, train=True)
+    train_loader = utils.data.DataLoader(dataset, shuffle=True)
 
-# train the model
-trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+    # init the autoencoder
+    autoencoder = AutoencoderKL(embed_dim=args.embed_dim, 
+                                z_channels=args.z_channel,
+                                resolution=args.resolution,
+                                in_channels=args.in_channels,
+                                out_ch=args.out_ch,
+                                ch = args.ch,
+                                ch_mult=args.ch_mult.split(','),
+                                num_res_blocks=args.num_res_blocks,
+                                dropout=args.dropout,
+                                monitor="val/rec_loss").to(device)
 
-# [optional] finish the wandb run, necessary in notebooks
-wandb.finish()
+    # initialise the wandb logger and name your wandb project
+    wandb_logger = WandbLogger(project='fmface_generator')
+
+    # add your batch size to the wandb config
+    wandb_logger.experiment.config["batch_size"] = args.batch_size
+
+    # model checkpoint custom
+    ckpt_callback = ModelCheckpoint(dirpath=args.checkpoint_path, every_n_train_steps=2000)
+
+    # pass wandb_logger to the Trainer 
+    trainer = pl.Trainer(logger=wandb_logger,callbacks=[ckpt_callback], benchmark= True, accumulate_grad_batches=2, accelerator="gpu" if device=='cuda' else 'cpu', devices=1)
+
+    # train the model
+    trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+
+    # [optional] finish the wandb run, necessary in notebooks
+    wandb.finish()
+
+if __name__ == "__main__":
+    arg = parse_args()
+    main(arg)
--- models/autoencoder.py
+++ models/autoencoder.py
@@ -285,9 +285,15 @@ from .ldm.modules.ema import LitEma
 
 class AutoencoderKL(pl.LightningModule):
     def __init__(self,
-                 ddconfig=None,
-                 lossconfig=None,
                  embed_dim=3,
+                 z_channels=3,
+                 resolution=256,
+                 in_channels=3,
+                 out_ch=3,
+                 ch=128,
+                 ch_mult=[1,2,4],
+                 num_res_blocks=2,
+                 dropout=0.0,
                  ckpt_path=None,
                  ignore_keys=[],
                  image_key="image",
@@ -296,13 +302,13 @@ class AutoencoderKL(pl.LightningModule):
                  ):
         super().__init__()
         self.image_key = image_key
-        self.encoder = Encoder(double_z= True, z_channels=3, resolution=256,in_channels=3,out_ch=3,ch=128,ch_mult=[ 1,2,4 ],
-                                num_res_blocks=2,attn_resolutions=[ ], dropout= 0.0)
-        self.decoder = Decoder(double_z= True, z_channels=3, resolution=256,in_channels=3,out_ch=3,ch=128,ch_mult=[ 1,2,4 ],
-                                num_res_blocks=2,attn_resolutions=[ ], dropout= 0.0)
+        self.encoder = Encoder(double_z= True, z_channels=z_channels, resolution=resolution,in_channels=in_channels,out_ch=out_ch,ch=ch,ch_mult=ch_mult,
+                                num_res_blocks=num_res_blocks,attn_resolutions=[ ], dropout= dropout)
+        self.decoder = Decoder(double_z= True, z_channels=z_channels, resolution=resolution,in_channels=in_channels,out_ch=out_ch,ch=ch,ch_mult=ch_mult,
+                                num_res_blocks=num_res_blocks,attn_resolutions=[ ], dropout= dropout)
         self.loss = LPIPSWithDiscriminator( disc_start= 50001, kl_weight= 0.000001, disc_weight= 0.5)
         self.learning_rate = 4.5e-6
-       # assert ddconfig["double_z"]
+        #assert double_z == True
         self.quant_conv = torch.nn.Conv2d(2*3, 2*embed_dim, 1)
         self.post_quant_conv = torch.nn.Conv2d(embed_dim, 3, 1)
         self.embed_dim = embed_dim
@@ -354,7 +360,7 @@ class AutoencoderKL(pl.LightningModule):
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         #inputs = self.get_input(batch, self.image_key)
-        inputs = batch.cuda()
+        inputs = batch.to(self.quant_conv.device)#.cuda()
         reconstructions, posterior = self(inputs)
 
         if optimizer_idx == 0:
