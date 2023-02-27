151584b7635d4160a193827bb1afff7070fb63e0
ta3h30nk1m <ta3h30nk1m@gmail.com>
2023-02-28 01:14:30 +0900

    add default config

---
+++ config/default_config.py
@@ -0,0 +1,33 @@
+AE_DEFAULT_CONFIG = {
+    'embed_dim': 3,
+    'z_channel': 3,
+    'resolution': 256,
+    'in_channels': 3,
+    'out_ch': 3,
+    'ch': 128,
+    'ch_mult': '1,2,4',
+    'num_res_blocks': 2,
+    'droutput': 0.0,
+}
+
+UNET_DEFAULT_CONFIG = {
+    'image_size': 256,
+    'in_channels': 3,
+    'out_channels': 3,
+    'model_channels': 224,
+    'attention_resolutions': '8,4,2',
+    'unet_num_res_blocks': 2,
+    'channel_mult': '1,2,3,4',
+    'num_head_channels': 32
+}
+
+LDM_DEFAULT_CONFIG = {
+    'linear_start': 0.0015,
+    'linear_end': 0.0195,
+    'log_every_t': 200,
+    'timesteps': 1000,
+    'first_stage_key': 'image',
+    'image_size': 256,
+    'channels': 3,
+    'monitor': 'val/loss_simple_ema',
+}
--- ldm_train.py
+++ ldm_train.py
@@ -2,15 +2,15 @@ import wandb
 import os, sys
 import torch
 from torch import optim, nn, utils, Tensor
-from data.base import FaceDataset, LitDataModule
-from models.autoencoder import AutoencoderKL
+from data.base import FaceDataset
+from models.ldm.ddpm import LatentDiffusion
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-from config.default_config import AE_DEFAULT_CONFIG
+from config.default_config import AE_DEFAULT_CONFIG, UNET_DEFAULT_CONFIG, LDM_DEFAULT_CONFIG
 
 def parse_args(argv=None, **kwargs):
     import argparse
@@ -37,22 +37,27 @@ def main(args):
     # setup data
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     dataset = FaceDataset(data_dir=args.data_path, train=True)
-    train_loader = utils.data.DataLoader(dataset, shuffle=True, batch_size=args.batch_size)
+    train_loader = utils.data.DataLoader(dataset, shuffle=True)
 
     # init the autoencoder
-    autoencoder = AutoencoderKL(embed_dim=args.embed_dim, 
-                                z_channels=args.z_channel,
-                                resolution=args.resolution,
-                                in_channels=args.in_channels,
-                                out_ch=args.out_ch,
-                                ch = args.ch,
-                                ch_mult=args.ch_mult.split(','),
-                                num_res_blocks=args.num_res_blocks,
-                                dropout=args.dropout,
-                                monitor="val/rec_loss").to(device)
+    autoencoder = LatentDiffusion( 
+        # ldm config
+        linear_start= args.Linear_start, Linear_end= args.Linear_end, log_every_t= args.Log_every_t,
+        timesteps= args.timesteps, first_stage_key= args.first_stage_key, image_size= args.image_size,
+        channels= args.channels, monitor= args.monitor,
+        #unet config
+        image_size= args.image_size, in_channels= args.in_channels, out_channels=args.out_ch,
+        model_channels= args.model_channels, attention_resolutions= args.attention_resoultions.split(','),
+        num_res_blocks= args.unet_num_res_blocks, channel_mult = args.channel_mult.split(','),
+        num_head_channels= args.num_head_channels,
+        #autoencoder config
+        embed_dim=args.embed_dim, z_channels=args.z_channel, resolution=args.resolution, #in_channels=args.in_channels, 
+        ch = args.ch, ch_mult=args.ch_mult.split(','), # out_ch=args.out_ch, 
+        num_res_blocks=args.num_res_blocks, dropout=args.dropout,
+                                ).to(device)
 
     # initialise the wandb logger and name your wandb project
-    wandb_logger = WandbLogger(project='fmface_generator', name='autoencoder_init')
+    wandb_logger = WandbLogger(project='fmface_generator')
 
     # add your batch size to the wandb config
     wandb_logger.experiment.config["batch_size"] = args.batch_size
@@ -61,14 +66,15 @@ def main(args):
     ckpt_callback = ModelCheckpoint(dirpath=args.checkpoint_path, every_n_train_steps=2000)
 
     # pass wandb_logger to the Trainer 
-    trainer = pl.Trainer(logger=wandb_logger,callbacks=[ckpt_callback], default_root_dir=args.checkpoint_path, benchmark= True, accumulate_grad_batches=4, 
-                         accelerator="gpu" if device=='cuda' else 'cpu', devices=1, max_epochs=args.epochs)
+    trainer = pl.Trainer(logger=wandb_logger,callbacks=[ckpt_callback], benchmark= True, accumulate_grad_batches=2, accelerator="gpu" if device=='cuda' else 'cpu', devices=1)
+
     # train the model
-    trainer.fit(model=autoencoder, train_dataloaders=train_loader, )#ckpt_path=args.checkpoint_file if args.resume_train else None)
+    trainer.fit(model=autoencoder, train_dataloaders=train_loader)
 
     # [optional] finish the wandb run, necessary in notebooks
     wandb.finish()
 
 if __name__ == "__main__":
-    arg = parse_args(sys.argv, AE_DEFAULT_CONFIG)
+    DEFAULT = dict(AE_DEFAULT_CONFIG.items()+UNET_DEFAULT_CONFIG.items()+LDM_DEFAULT_CONFIG.items())
+    arg = parse_args(sys.argv, DEFAULT)
     main(arg)
